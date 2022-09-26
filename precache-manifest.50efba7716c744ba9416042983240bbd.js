55e226bc6a5cfbbfde532b3b487cf597e0c3e44f
ecruz17 <ecruz17@ucol.mx>
2022-09-25 23:06:25 -0500

    Updates

---
+++ precache-manifest.50efba7716c744ba9416042983240bbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "22fb13348967b3981f83",
+    "url": "/la-ferreteria-app/static/css/main.d9d697d0.chunk.css"
+  },
+  {
+    "revision": "22fb13348967b3981f83",
+    "url": "/la-ferreteria-app/static/js/main.22fb1334.chunk.js"
+  },
+  {
+    "revision": "94421d0c55535ff39cc4",
+    "url": "/la-ferreteria-app/static/js/1.94421d0c.chunk.js"
+  },
+  {
+    "revision": "080ba439773c381890fd",
+    "url": "/la-ferreteria-app/static/css/2.10e0d208.chunk.css"
+  },
+  {
+    "revision": "080ba439773c381890fd",
+    "url": "/la-ferreteria-app/static/js/2.080ba439.chunk.js"
+  },
+  {
+    "revision": "a35fb2aa4d8b3b5c8a27",
+    "url": "/la-ferreteria-app/static/js/runtime~main.a35fb2aa.js"
+  },
+  {
+    "revision": "7458c5b8c56b89bfee10c4c8071c6da3",
+    "url": "/la-ferreteria-app/index.html"
+  }
+];
